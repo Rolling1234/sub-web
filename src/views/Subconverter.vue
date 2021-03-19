56dc9f6545d13c8be6a99f618566334bffae4add
Liwanqian <lhjsj520@gmail.com>
2021-03-19 11:25:12 +0800

    add short link

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -15,7 +15,7 @@
               style="margin-left: 15px"
               @click="gotoTgChannel"
             />
-            <div style="display: inline-block; position:absolute; right: 15px">
+            <div style="display: inline-block; position: absolute; right: 15px">
               {{ backendVersion }}
             </div>
           </div>
@@ -411,9 +411,9 @@ export default {
           自动判断客户端: "auto",
         },
         shortTypes: {
+          "tlp.pw": "https://tlp.pw/short",
           "suo.yt": "https://suo.yt/short",
           "v1.mk": "https://v1.mk/short",
-          "d1.mk": "https://d1.mk/short",
           "dlj.tf": "https://dlj.tf/short",
           "sub.cm": "https://sub.cm/short",
         },
@@ -576,7 +576,7 @@ export default {
         sourceSubUrl: "",
         clientType: "",
         customBackend: "https://shaguo.link/sub?",
-        shortType: "https://suo.yt/short",
+        shortType: "https://tlp.pw/short",
         remoteConfig:
           "https://raw.githubusercontent.com/Dashaguo/My-rules/main/Myrules.ini",
         excludeRemarks: "",
